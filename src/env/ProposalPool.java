3c9487e4ffcac2684b692b5855616a5c9cdb1413
Rafael-Ramildes-Ferreira <rafael.r.ferreira@grad.ufsc.br>
2024-09-20 09:12:06 -0300

    Now the proposal pool closes automatically

--- src/env/ProposalPool.java
+++ src/env/ProposalPool.java
@@ -6,20 +6,50 @@ import cartago.*;
 
 
 public class ProposalPool extends Artifact {
-	void init() {
+	public void init() {
 		defineObsProperty("status", "not started");
 		System.out.println("[pool] Proposal pool created");
 	}
 
 	@OPERATION
-	void open(int id, int m) {
+	public void open(int id, int m) {
 		updateObsProperty("status", "open");
 		System.out.println("[pool] Proposal pool started");
+
+		execInternalOp("countdown");
 	}
 
 	@OPERATION
-	void propose(Object proposer) {
-		System.out.println("[pool] Receive a propose from " + proposer);
+	public void close() {
+		updateObsProperty("status", "closed");
+		System.out.println("[pool] Proposal pool has ended");
+	}
+
+	@OPERATION
+	public void propose(Object proposer) {
+		if(getObsProperty("status").getValue() == "open"){
+			System.out.println("[pool] Receive a propose from " + proposer);
+		} else {
+			System.out.println("[pool] Refuse a propose from " + proposer + " due to timeout");
+		} 
+	}
+
+	
+	@INTERNAL_OPERATION
+	private void countdown() {
+		System.out.println("[pool] Proposal pool is counting down");
+  
+		try{
+	  		Thread.sleep(2000);
+		} catch (InterruptedException e) {
+            Thread.currentThread().interrupt();  // Restore interrupted status
+		}
+
+		execInternalOp("close");
+		// Apparently operations get buffered and can't execute simultaneously,
+		// which means: proposals do not get stored while this countdown is happening.
+		// Calling close directly (as below) would close before any one could propose
+		// close();
 	}
 
 }
